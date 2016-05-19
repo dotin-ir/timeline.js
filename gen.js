15482b2e15b8e5469a5e14b85bd8e17af0acecb4
m.j.izadi <m.j.izadi@dotin.ir>
2016-05-19 20:08:24 +0430

    Refactor code

--- gen.js
+++ gen.js
@@ -1,4 +1,4 @@
-var persian = ['۰', '۱', '۲',  '۳',  '۴',  '۵',  '۶',  '۷',  '۸', '۹'];
+var persianDigits = ['۰', '۱', '۲',  '۳',  '۴',  '۵',  '۶',  '۷',  '۸', '۹'];
 var daysOfWeek = ['شنبه', 'یک‌شنبه', 'دوشنبه',  'سه‌شنبه',  'چهارشنبه',  'پنج‌شنبه',  'جمعه'];
 
 $( document ).ready(function() {
@@ -18,119 +18,110 @@ function addTitle(title) {
 }
 
 function addSprints(data) {
-	var gD = new Date();
-	var jD = toJalaali(gD.getFullYear(), gD.getMonth()+1, gD.getDate());
-	var jMonth = jD.jm;
-	var jDay = jD.jd;
-
 	var sprints = $('<table></table>').attr('cellspacing', 0);
 
-	var colHeader = $('<colgroup></colgroup>');
-
-	var months = data.season.months;
-	var dayOfWeek = data.season.startsOnDayOfWeek;
-	var weekend = data.season.weekend;
-
-	var weekDays = $('<tr></tr>').append('<td></td>');
-
-	colHeader.append($('<col/>'));
-	for (var i=0; i<months.length; i++) {
-		var month = months[i];
-		for (var j=1; j<month.numOfDays+1; j++) {
-			var col = $('<col/>').text(j);
-			if (dayOfWeek % 7 == weekend || $.inArray(j, month.holidays) != -1) {
-				col.addClass('offDay');
-			}
-			if (month.id == jMonth && j == jDay) {
-				col.addClass('today');
-			}
-			colHeader.append(col);
-
-			weekDays.append($('<td></td>').addClass('vertical-text dayOfWeek').text(daysOfWeek[dayOfWeek%7]));
-
-			dayOfWeek++;
-		}
-	}
-	sprints.append(colHeader);
-
-	var monthNames = $('<tr></tr>').addClass('monthNames');
-	var days = $('<tr></tr>').addClass('dayNumbers');
-
-	monthNames.append($('<th></th>').text('ماه'));
-	days.append($('<td></td>').text('روز'));
-
-	for (var i=0; i<months.length; i++) {
-		var month = months[i];
-
-		var monthName = $('<th></th>').text(month.name).attr('colspan', month.numOfDays);
-		if (month.id != 1) {
-			monthName.addClass('firstDay');
-		}
-		monthNames.append(monthName);
-
-		for (var j=1; j<=month.numOfDays; j++) {
-			var day = $('<td></td>').text(convert(j)).addClass('vertical-text');
-			if (month.id != 1 && j == 1) {
-				day.addClass('firstDay');
-			}
-			days.append(day);
-		}
-	}
-
-	sprints.append(monthNames);
-	sprints.append(days);
-
-	for (var i=0; i<data.teams.length; i++) {
-		var team = data.teams[i];
-
-		var sprintRow = $('<tr></tr>');
-		var nameCell = $('<td></td>').text(team.name).addClass('vertical-text');
-		nameCell.addClass('team-name');
-		nameCell.attr('rowspan', Math.max(1, team.sprints.length));
-		nameCell.attr('style', 'background-color: ' + team.color + ';');
-		sprintRow.append(nameCell);
-		//sprints.append(teamRow);
-
-		for (var j=0; j<team.sprints.length; j++) {
-			var planning = team.sprints[j].planning;
-			var retrospective = team.sprints[j].retrospective;
-			for (m = 0; m < months.length; m++) {
-				monthId = months[m].id;
-				for (day = 1; day <= months[m].numOfDays; day++) {
-					var dayCol = $('<td></td>');
-					if (monthId != 1 && day == 1) {
-						dayCol.addClass('firstDay');
-					}
-					
-					if ((planning.monthId == monthId && planning.day <= day
-													|| planning.monthId < monthId)
-							&& (retrospective == "infinity"
-											|| retrospective.monthId == monthId
-															&& retrospective.day >= day
-											|| retrospective.monthId > monthId))
-					{
-						dayCol.attr('style', 'background-color: ' + team.color + ';');
-						if (planning.monthId == monthId && planning.day == day) {
-							dayCol.append($('<i></i>').addClass('fa fa-check-square-o'));
-						}
-						if (retrospective.monthId == monthId && retrospective.day == day) {
-							dayCol.append($('<i></i>').addClass('fa fa-user-md'));
-						}
-					}
-					sprintRow.append(dayCol);
-				}
-			}
-			sprints.append(sprintRow);
-			sprintRow = $('<tr></tr>');
-		}
-	}
-	sprints.append(weekDays);
+	appendHeadersToSprints(sprints, data.season);
+	appendTeamsToSprints(sprints, data.teams, data.season.months);
+	appendFootersToSprints(sprints, data.season);
 
 	$('#sprints').append(sprints);
 }
 
-function convertDigit(number) {
-	return persian[number];
+function appendHeadersToSprints(sprints, season) {
+	sprints.append(createHeaderColumns(season.months, season.startsOnDayOfWeek, season.weekend));
+	sprints.append(createMonthsHeader(season.months));
+	sprints.append(createDaysHeader(season.months));
+}
+
+function createHeaderColumns(months, startsOnDayOfWeek, weekend) {
+	var colHeader = $('<colgroup></colgroup>').append(createTitleRowColumn());
+
+	var dayOfWeek = startsOnDayOfWeek;
+	for (var i=0; i<months.length; i++) {
+		var month = months[i];
+		appendMonthToColHeader(colHeader, month.id, month.numOfDays, dayOfWeek, weekend, month.holidays);
+		dayOfWeek = advanceDayOfWeek(dayOfWeek, month.numOfDays);
+	}
+	return colHeader;
+}
+
+function createTitleRowColumn() {
+	return $('<col/>');
+}
+
+function appendMonthToColHeader(colHeader, monthId, numOfDays, firstDayOfWeek, weekend, holidays) {
+	dayOfWeek = firstDayOfWeek;
+	for (var dayOfMonth=1; dayOfMonth<=numOfDays; dayOfMonth++) {
+		colHeader.append(createColumnForDay(monthId, dayOfMonth, dayOfWeek, weekend, holidays));
+		dayOfWeek++;
+		dayOfWeek %= 7;
+	}
+}
+
+function createColumnForDay(monthId, dayOfMonth, dayOfWeek, weekend, holidays) {
+	var col = $('<col/>').text(dayOfMonth);
+	if (isWeekend(dayOfWeek, weekend) || isHolliday(dayOfMonth, holidays)) {
+		col.addClass('offDay');
+	}
+	if (isToday(monthId, dayOfMonth)) {
+		col.addClass('today');
+	}
+	return col;
+}
+
+function isWeekend (dayOfWeek, weekend) {
+	return dayOfWeek == weekend;
+}
+
+function isHolliday(dayOfMonth, holidays) {
+	return $.inArray(dayOfMonth, holidays) != -1;
+}
+
+function isToday(month, day) {
+	var gregorian = new Date();
+	var jalaliDate = toJalaali(gregorian.getFullYear(), gregorian.getMonth()+1, gregorian.getDate());
+	return month == jalaliDate.jm && day == jalaliDate.jd;
+}
+
+function createMonthsHeader(months) {
+	var monthNamesHeader = $('<tr></tr>').addClass('monthNames').append($('<th></th>').text('ماه'));
+
+	for (var i=0; i<months.length; i++) {
+		monthNamesHeader.append(createMonthName(months[i]));
+	}
+	return monthNamesHeader;
+}
+
+function createMonthName(month) {
+	var monthName = $('<th></th>').text(month.name).attr('colspan', month.numOfDays);
+	if (month.id != 1) {
+		monthName.addClass('monthSeparator');
+	}
+	return monthName;
+}
+
+function createDaysHeader(months) {
+	var daysHeader = $('<tr></tr>').addClass('dayNumbers');
+	daysHeader.append($('<td></td>').text('روز'));
+	for (var i=0; i<months.length; i++) {
+		appendDaysHeaderForMonth(daysHeader, months[i]);
+	}
+
+	return daysHeader;
+}
+
+function appendDaysHeaderForMonth(daysHeader, month) {
+	for (var dayOfMonth=1; dayOfMonth<=month.numOfDays; dayOfMonth++) {
+		daysHeader.append(createDayHeader(month.id, dayOfMonth));
+	}
+}
+
+function createDayHeader(monthId, dayOfMonth) {
+	var day = $('<td></td>').text(convert(dayOfMonth)).addClass('vertical-text');
+	if (monthId != 1 && dayOfMonth == 1) {
+		day.addClass('monthSeparator');
+	}
+	return day;
 }
 
 function convert(number) {
@@ -142,3 +133,107 @@ function convert(number) {
 	return result;
 }
 
+function convertDigit(number) {
+	return persianDigits[number];
+}
+
+function appendTeamsToSprints(sprints, teams, months) {
+	for (var i=0; i<teams.length; i++) {
+		appendTeamSprints(sprints, teams[i], months);
+	}
+}
+
+function appendTeamSprints(sprints, team, months) {
+
+	for (var sprintId=0; sprintId<team.sprints.length; sprintId++) {
+		sprints.append(createSprintRow(team, sprintId, months));
+	}
+}
+
+function createSprintRow(team, sprintId, months) {
+	var sprintRow = $('<tr></tr>');
+	if (sprintId == 0) {
+		sprintRow.append(createNameCell(team));
+	}
+
+	for (m = 0; m < months.length; m++) {
+		appendMonthForSprint(sprintRow, months[m], team.sprints[sprintId], team.color);
+	}
+	return sprintRow;
+}
+
+function createNameCell(team) {
+	var nameCell = $('<td></td>').text(team.name).addClass('vertical-text');
+	nameCell.addClass('team-name');
+	nameCell.attr('rowspan', Math.max(1, team.sprints.length));
+	nameCell.attr('style', 'background-color: ' + team.color + ';');
+	return nameCell;
+}
+
+function appendMonthForSprint(sprintRow, month, sprint, teamColor) {
+	for (day = 1; day <= month.numOfDays; day++) {
+		sprintRow.append(createDayCell(month.id, day, sprint.planning, sprint.retrospective, teamColor));
+	}
+}
+
+function createDayCell(monthId, day, planning, retrospective, teamColor) {
+	var dayCell = $('<td></td>');
+	if (monthId != 1 && day == 1) {
+		dayCell.addClass('monthSeparator');
+	}
+	
+	if (isDayInSprint(monthId, day, planning, retrospective)) {
+		dayCell.attr('style', 'background-color: ' + teamColor + ';');
+		applyDayIcons(dayCell, monthId, day, planning, retrospective);
+	}
+	return dayCell;
+}
+
+function applyDayIcons(dayCell, monthId, day, planning, retrospective) {
+	if (isPlanningDay(monthId, day, planning)) {
+		dayCell.append($('<i></i>').addClass('fa fa-check-square-o'));
+	}
+	if (isRetrospectiveDay(monthId, day, retrospective)) {
+		dayCell.append($('<i></i>').addClass('fa fa-user-md'));
+	}
+}
+
+function isPlanningDay(monthId, day, planning) {
+	return planning.monthId == monthId && planning.day == day;
+}
+
+function isRetrospectiveDay(monthId, day, retrospective) {
+	return retrospective.monthId == monthId && retrospective.day == day;
+}
+
+function isDayInSprint(monthId, day, planning, retrospective) {
+	if (planning.monthId == monthId && planning.day <= day || planning.monthId < monthId) {
+		if (retrospective == "infinity") {
+	   		return true;
+		}
+		return retrospective.monthId == monthId && retrospective.day >= day || retrospective.monthId > monthId;
+	}
+	return false;
+}
+
+function appendFootersToSprints(sprints, season) {
+	sprints.append(createDaysOfWeekRow(season.months, season.startsOnDayOfWeek));
+}
+
+function createDaysOfWeekRow(months, startsOnDayOfWeek) {
+	var daysOfWeek = $('<tr></tr>').append('<td></td>');
+
+	var dayOfWeek = startsOnDayOfWeek;
+	for (var i=0; i<months.length; i++) {
+		var month = months[i];
+		for (var j=1; j<month.numOfDays+1; j++) {
+			daysOfWeek.append($('<td></td>').addClass('vertical-text dayOfWeek').text(daysOfWeek[dayOfWeek]));
+			dayOfWeek = advanceDayOfWeek(dayOfWeek, 1);
+		}
+	}
+	return daysOfWeek;
+}
+
+function advanceDayOfWeek(dayOfWeek, amount) {
+	return (dayOfWeek + amount) % 7;
+}
